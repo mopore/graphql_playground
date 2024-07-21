780cd5dc2efb1a5a96422586f76feb2ea8f674ed
mopore <jni@mopore.org>
2024-07-21 13:33:13 +0200

    Add an example to add an entity.

--- bun_apollo_server/main.ts
+++ bun_apollo_server/main.ts
@@ -14,10 +14,19 @@ const schemaDef = `#graphql
         books: [Book]
     }
 
+    type Mutation {
+        addBook(title: String!, author: String!): [Book]
+    }
 `;
 
+interface Book {
+    id: string;
+    title: string;
+    author: string;
+}
 
-const books = [
+
+const books: Book[] = [
     {
         id: "1",
         title: "The Awakening",
@@ -32,8 +41,21 @@ const books = [
 
 const resolvers = {
     Query: {
-        books: () => books,
+        books: (): Book[] => books,
     },
+    Mutation: {
+        addBook: (_: any, args: any): Book[] => {
+            const newBook = {
+                id: String(books.length + 1),
+                title: args.title,
+                author: args.author,
+                }
+            books.push(newBook);
+            console.log(`Added book: ${newBook.title} by ${newBook.author}`);
+            console.log(`New length: ${books.length}`);
+            return books;
+        }
+    }
 };
 
 const server = new ApolloServer({ typeDefs: schemaDef, resolvers });
--- go_client/cmd/cli/main.go
+++ go_client/cmd/cli/main.go
@@ -1,46 +1,82 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    "log"
+	"context"
+	"fmt"
+	"log"
 
-    "github.com/machinebox/graphql"
+	"github.com/machinebox/graphql"
 )
 
 type Book struct {
-    ID    string `json:"id"`
-    Title  string `json:"title"`
-    Author string `json:"author"`
+	ID     string `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
 }
 
-func queryBooks(client *graphql.Client) {
-    req := graphql.NewRequest(`
+func queryBooks(client *graphql.Client) []Book {
+	req := graphql.NewRequest(`
         query GetBooks {
-          books {
-            id
-            title
-            author
-          }
+            books {
+                id
+                title
+                author
+            }
         }
     `)
 
-    var respDate struct {
-        Books []Book `json:"books"`
-    }
+	var respData struct {
+		Books []Book `json:"books"`
+	}
 
-    if err := client.Run(context.Background(), req, &respDate); err != nil {
-        log.Fatal(err)
-    }
+	if err := client.Run(context.Background(), req, &respData); err != nil {
+		log.Fatal(err)
+	}
 
+    return respData.Books
+}
+
+func addBook(client *graphql.Client) []Book {
+	req := graphql.NewRequest(`
+        mutation AddBook($title: String!, $author: String!) {
+            addBook(title: $title, author: $author) {
+                id
+                title
+                author
+            }
+        }
+    `)
+	req.Var("title", "The Art of Computer Programming")
+	req.Var("author", "Donald E. Knuth")
+
+	var respData struct {
+		Books []Book `json:"addBook"`
+	}
+
+	if err := client.Run(context.Background(), req, &respData); err != nil {
+		log.Fatal(err)
+	}
+
+    return respData.Books
+}
+
+func printBooks(books []Book) {
     fmt.Println("Books:")
-    for _, book := range respDate.Books {
+    fmt.Printf(("Length of books: %d\n"), len(books))
+    for _, book := range books {
         fmt.Printf("%s, Title: %s, Author: %s\n", book.ID, book.Title, book.Author)
     }
 }
 
 
 func main() {
-    client := graphql.NewClient("http://localhost:4000/graphql")
-    queryBooks(client)
+	client := graphql.NewClient("http://localhost:4000/graphql")
+
+    fmt.Println("Querying books...")
+    books := queryBooks(client)
+    printBooks(books)
+
+    fmt.Println("Adding a new book...")
+    books = addBook(client)
+    printBooks(books)
 }
